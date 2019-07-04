25218456b6963834612bac6c0bf6270ceac2e222
memmsc <xiongfei>
2019-07-04 17:32:50 +0800

    push

---
+++ vue-element-admin/src/router/modules/system.js
@@ -0,0 +1,37 @@
+/** When your routing table is too long, you can split it into small modules **/
+
+import Layout from '@/layout'
+
+const systemRouter = {
+  path: '/system',
+  component: Layout,
+  redirect: '/system',
+  name: 'System',
+  meta: {
+    title: '系统管理',
+    icon: 'list'
+  },
+  children: [
+    {
+      path: 'system-table',
+      component: () => import('@/views/system/user/index'),
+      name: 'UserManager',
+      meta: { title: '用户管理' }
+    }
+    ,
+    {
+      path: 'role-table',
+      component: () => import('@/views/system/role/index'),
+      name: 'RoleManager',
+      meta: { title: '角色管理' }
+    }
+    //,
+    // {
+    //   path: 'log-table',
+    //   component: () => import('@/views/system/log/index'),
+    //   name: 'SystemLog',
+    //   meta: { title: '系统日志' }
+    // }
+  ]
+}
+export default systemRouter
---
+++ vue-element-admin/src/router/modules/table.js
@@ -0,0 +1,23 @@
+/** When your routing table is too long, you can split it into small modules **/
+
+import Layout from '@/layout'
+
+const tableRouter = {
+  path: '/table',
+  component: Layout,
+  redirect: '/table/complex-table',
+  name: 'Table',
+  meta: {
+    title: '表单',
+    icon: 'table'
+  },
+  children: [
+    {
+      path: 'dynamic-table',
+      component: () => import('@/views/table/dynamic-table/index'),
+      name: 'DynamicTable',
+      meta: { title: '动态表单' }
+    }
+  ]
+}
+export default tableRouter
